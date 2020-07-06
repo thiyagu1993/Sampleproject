b2785cec182974ed2cdab9b96f06dfb9cc9b2721
Thiyagarajan_lenovo <rthiyagara@gmail.com>
2020-07-06 09:43:06 +0530

    lenovo commit

--- sample/src/first/sample1.java
+++ sample/src/first/sample1.java
@@ -4,9 +4,9 @@ public class sample1 {
 
 	public static void main(String[] args){
 		
-		System.out.println("Running....");
+		System.out.println("Run");
 		//running fine
-			
+	//lenovo		
 	}
 	
 }
